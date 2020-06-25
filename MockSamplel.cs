47a5d7744722ac047f66d7152dd61a080f26f87f
Kamal-Sharma <Kamal.Sharma@coxautoinc.com>
2020-06-25 18:00:46 +0530

    first commit

---
+++ MockSamplel.cs
@@ -0,0 +1,57 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTestLib
+{
+    public class MockSamplel
+    {
+
+        [Fact]
+        public void Test1()
+        {
+            var mock = new Mock<IFoo>();
+
+
+            #region Property
+            mock.Setup(x => x.Name).Returns("foo");
+            mock.SetupProperty(f => f.Name, "foo");
+            #endregion
+
+
+
+
+            #region Methods
+            mock.Setup(x => x.DoSomething("foo")).Returns(true);
+            mock.Setup(x => x.DoSomethingStringy(It.IsAny<string>())).Returns((string s) => s.ToLower());
+            mock.Setup(x => x.DoSomething(It.IsAny<int>(),It.IsAny<string>())).Returns((int x, string s) => (x==1 && s=="foo"));
+            var outString = "ack";
+            // TryParse will return true, and the out argument will return "ack", lazy evaluated
+            mock.Setup(foo => foo.TryParse("ping", out outString)).Returns(true);
+            mock.Setup(foo => foo.DoSomething("error")).Throws<OutOfMemoryException>();
+            mock.Setup(foo => foo.Add(It.IsInRange<int>(0, 10, Moq.Range.Inclusive))).Returns(true);
+            #endregion
+
+
+            #region Events
+
+
+            #endregion
+
+            IFoo foo = mock.Object;
+            Assert.Equal("foo", foo.Name);
+            Assert.True(foo.DoSomething("foo"));
+            Assert.True(foo.DoSomething(1,"foo"));
+            string s = "8";
+            Assert.True(foo.TryParse("ping", out s));
+            Assert.True(s== "ack");
+            Assert.True(foo.DoSomething("error"));
+            Assert.Equal("foo", foo.DoSomethingStringy("FOO"));
+        }
+
+
+
+    }
+}
---
+++ TestCases.cs
@@ -0,0 +1,154 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace UnitTestLib
+{
+    public class TestCases
+    {
+        [Fact]
+        public void Task_Add_TwoNumber()
+        {
+            // Arrange  
+            var num1 = 2.9;
+            var num2 = 3.1;
+            var expectedValue = 6;
+
+            // Act  
+            var sum = MathOperation.Add(num1, num2);
+
+            //Assert  
+            Assert.Equal(expectedValue, sum);
+        }
+
+        [Fact]
+        public void Task_Subtract_TwoNumber()
+        {
+            // Arrange  
+            var num1 = 2.9;
+            var num2 = 3.1;
+            var expectedValue = -0.2;
+
+            // Act  
+            var sub = Math.Round(MathOperation.Subtract(num1, num2),2);
+
+            //Assert  
+            Assert.Equal(expectedValue, sub);
+        }
+
+        [Fact]
+        public void Task_Multiply_TwoNumber()
+        {
+            // Arrange  
+            var num1 = 2.9;
+            var num2 = 3.1;
+            var expectedValue = 8.99;
+
+            // Act  
+            var mult = MathOperation.Multiply(num1, num2);
+
+            //Assert  
+            Assert.Equal(expectedValue, mult);
+        }
+
+        [Theory]
+        [InlineData(5, 2, 3)]
+        //[InlineData(-4, -6, -10)]
+        //[InlineData(-2, 2, 0)]
+        //[InlineData(int.MinValue, -1, int.MaxValue)]
+        public void CanSubtractTheory(int value1, int value2, int expected)
+        {
+          
+
+            var result = MathOperation.Subtract(value1, value2);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [ClassData(typeof(TestData))]
+        public void CanAddTheory(int value1, int value2, int expected)
+        {
+            var result = MathOperation.Add(value1, value2);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        [MemberData(nameof(GetData),parameters:2)]
+        [MemberData(nameof(Data),MemberType = typeof(TestData))]
+        public void CanAddTheoryMemberDataProperty(int value1, int value2, int expected)
+        {
+             
+
+            var result = MathOperation.Add(value1, value2);
+
+            Assert.Equal(expected, result);
+        }
+
+        public static IEnumerable<object[]> Data =>
+            new List<object[]>
+            {
+            new object[] { 1, 2, 3 },
+            new object[] { -4, -6, -10 },
+            new object[] { -2, 2, 0 },
+            new object[] { int.MinValue, -1, int.MaxValue },
+            };
+
+        public static IEnumerable<object[]> GetData(int numTests)
+        {
+            var allData = new List<object[]>
+        {
+            new object[] { 1, 2, 3 },
+            new object[] { -4, -6, -10 },
+            new object[] { -2, 2, 0 },
+            new object[] { int.MinValue, -1, int.MaxValue },
+        };
+
+            return allData.Take(numTests);
+        }
+
+
+        [Fact]
+        public void Task_Divide_TwoNumber()
+        {
+            // Arrange  
+            var num1 = 2.9;
+            var num2 = 3.1;
+            var expectedValue = 0.94; //Rounded value  
+
+            // Act  
+            var div =  Math.Round(MathOperation.Divide(num1, num2),2);
+
+            //Assert  
+            Assert.Equal(expectedValue, div);
+        }
+
+        private class TestData : System.Collections.Generic.IEnumerable<object[]>
+        {
+            public static IEnumerable<object[]> Data =>
+           new List<object[]>
+           {
+            new object[] { 1, 2, 3 },
+            new object[] { -4, -6, -10 },
+            new object[] { -2, 2, 0 },
+            new object[] { int.MinValue, -1, int.MaxValue },
+           };
+
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] { 1,2,3};
+                yield return new object[] { 1, 2, 5 };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+    }
+}
